4ceb6f18c603a2fb93b2d5ba400c6887227a4210
Rishi SHeth <sheth19@purdue.edu>
2020-08-29 18:17:48 -0400

    First commit

---
+++ build/precache-manifest.afc25b7e487e6a527a83823ae817343f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea509f24f50b1b9081a1a23b03094d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70cb6035e6c71e101131",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "39c1f1364e9d0e7de688",
+    "url": "/static/css/main.2bc35f05.chunk.css"
+  },
+  {
+    "revision": "70cb6035e6c71e101131",
+    "url": "/static/js/2.203cd0d4.chunk.js"
+  },
+  {
+    "revision": "a87d9bd7ada1c570d0f3802923967630",
+    "url": "/static/js/2.203cd0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c1f1364e9d0e7de688",
+    "url": "/static/js/main.c5d7fccf.chunk.js"
+  },
+  {
+    "revision": "27ac43a3e96c3e599aa1",
+    "url": "/static/js/runtime-main.5210d27b.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
